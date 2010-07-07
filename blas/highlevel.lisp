dec137898d72721b86326a46adb39e2b9900bee8
allchemist <hohlovivan@gmail.com>
2010-07-08 00:13:17 +0400

    blas tests, gemm asserts

--- blas/highlevel.lisp
+++ blas/highlevel.lisp
@@ -14,7 +14,8 @@
 
 (defun e-norm (X)
   (sb-sys:with-pinned-objects (X)
-    (funcall (with-function-choice 'nrm2 (array-element-type X))
+    (funcall (with-float-type-choice (array-element-type X)
+	       #'%snrm2 #'%dnrm2 #'%scnrm2 #'%dznrm2)
 	     (array-total-size X) (array-sap X) 1)))
 
 (defun normalize (X)
@@ -104,7 +105,7 @@
 	 (array-sap dest) 1)
 	dest))))
 
-(defun ger (X Y &key dest (alpha 0) (conj :noconj))
+(defun ger (X Y &key dest (alpha 1) (conj :noconj))
   (let ((type (array-element-type X)))
     (if dest
 	(assert (and (= (dim0 X) (dim0 dest))
@@ -112,7 +113,7 @@
 		nil "Improper dimensions for ger")
 	(setf dest (make-matrix (list (dim0 X) (dim0 Y)) :element-type type)))
     (sb-sys:with-pinned-objects (dest X Y alpha)
-      (funcall (with-function-choice 'ger type)
+      (funcall (with-float-type-choice  type #'sger #'dger #'cger #'zger)
 	       dest X Y (coerce alpha type) conj)
       dest)))
 
@@ -155,7 +156,7 @@
 	 (array-sap X) 1 (complex-sap (coerce beta type)) (array-sap dest) 1)
 	dest))))
 
-(defun syr (X &key dest (alpha 0) (uplo :upper))
+(defun syr (X &key dest (alpha 1) (uplo :upper))
   (let ((dim (dim0 X)))
     (let ((type (array-element-type X)))
       (if dest
@@ -168,7 +169,7 @@
 		 (array-sap X) 1 (array-sap dest) dim)
 	dest))))
 
-(defun her (X &key dest (alpha #C(0.0 0.0)) (uplo :upper))
+(defun her (X &key dest (alpha #C(1.0 0.0)) (uplo :upper))
   (let ((dim (dim0 X)))
     (let ((type (array-element-type X)))
       (if dest
@@ -216,16 +217,16 @@
 
 ;; X contents is substituted!
 (defun tpmv (Ap X &key (uplo :upper) (transA :notrans))
-  (let ((dim (dim0 Ap)))
-    (assert (= dim (dim1 Ap) (dim0 X)) nil "Improper dimesions for tpmv")
+  (let ((dim (ldm Ap)))
+    (assert (= dim (dim0 X)) nil "Improper dimesions for tpmv")
     (sb-sys:with-pinned-objects (Ap X)
       (funcall (with-function-choice 'tpmv (pmatrix-element-type Ap))
-	       'CBlasRowMajor uplo transA :nonunit dim (pmatrix-sap Ap) dim (array-sap X) 1)
+	       'CBlasRowMajor uplo transA :nonunit dim (pmatrix-sap Ap) (array-sap X) 1)
       X)))
 
 (defun spmv (Ap X &key dest (alpha 1) (beta 0) (uplo :upper))
-  (let ((dim (dim0 Ap)))
-    (assert (= dim (dim1 Ap) (dim0 X)) nil "Improper dimensions for spmv")
+  (let ((dim (ldm Ap)))
+    (assert (= dim (dim0 X)) nil "Improper dimensions for spmv")
     (let ((type (pmatrix-element-type Ap)))
       (assert (not (subtypep type 'complex)) nil "Symmetric matrix should be real")
       (if dest
@@ -234,13 +235,13 @@
       (sb-sys:with-pinned-objects (Ap X dest)
 	(funcall
 	 (with-function-choice 'spmv type)
-	 'CBlasRowMajor uplo dim (coerce alpha type) (pmatrix-sap Ap) dim
+	 'CBlasRowMajor uplo dim (coerce alpha type) (pmatrix-sap Ap)
 	 (array-sap X) 1 (coerce beta type) (array-sap dest) 1)
 	dest))))
 
 (defun hpmv (Ap X &key dest (alpha #C(1.0 0.0)) (beta #C(0.0 0.0)) (uplo :upper))
-  (let ((dim (dim0 Ap)))
-    (assert (= dim (dim1 Ap) (dim0 X)) nil "Improper dimensions for hpmv")
+  (let ((dim (ldm Ap)))
+    (assert (= dim (dim0 X)) nil "Improper dimensions for hpmv")
     (let ((type (pmatrix-element-type Ap)))
       (assert (subtypep type 'complex) nil "Hermitian matrix should be complex")
       (if dest
@@ -248,12 +249,12 @@
 	  (setf dest (make-matrix dim :element-type type)))
       (sb-sys:with-pinned-objects (Ap X dest alpha beta)
 	(funcall
-	 (with-function-choice 'hemv type)
-	 'CBlasRowMajor uplo dim (complex-sap (coerce alpha type)) (pmatrix-sap Ap) dim
+	 (with-function-choice 'hpmv type)
+	 'CBlasRowMajor uplo dim (complex-sap (coerce alpha type)) (pmatrix-sap Ap)
 	 (array-sap X) 1 (complex-sap (coerce beta type)) (array-sap dest) 1)
 	dest))))
 
-(defun spr (X &key dest (alpha 0) (uplo :upper))
+(defun spr (X &key dest (alpha 1) (uplo :upper))
   (let ((dim (dim0 X)))
     (let ((type (array-element-type X)))
       (if dest
@@ -269,7 +270,7 @@
 		 (array-sap X) 1 (pmatrix-sap dest) dim)
 	dest))))
 
-(defun hpr (X &key dest (alpha #C(0.0 0.0)) (uplo :upper))
+(defun hpr (X &key dest (alpha #C(1.0 0.0)) (uplo :upper))
   (let ((dim (dim0 X)))
     (let ((type (array-element-type X)))
       (if dest
