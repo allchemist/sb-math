49c41a2a6f8a4b21b9c5b6687c7768dbe34c456f
allchemist <hohlovivan@gmail.com>
2010-07-09 16:57:29 +0400

    yet another commit

--- lapack/lowlevel.lisp
+++ lapack/lowlevel.lisp
@@ -25,6 +25,8 @@
 	   (A array) (lda integer dim) (ipiv array) (B array)
 	   (ldb integer dim) (info integer)))
 
+#|
+;not for complex float data
 (define-lapack-wrapper gesvd
     :return info
   :lets ((dim0 (dim1 A)) (dim1 (dim0 A)))
@@ -35,3 +37,16 @@
 	   (A array) (lda integer dim0) (S array) (u array)
 	   (ldu integer (dim0 u)) (vt array) (ldvt integer (dim0 vt)) (work array)
 	   (lwork integer (dim0 work)) (complex-only (rwork array)) (info integer 0)))
+|#
+
+(define-lapack-wrapper gesvd
+    :return info
+  :lets ((dim0 (dim0 A)) (dim1 (dim1 A)))
+  :array-args (A S U VT work (complex-only rwork))
+  :rest-args (jobu jobvt)
+  :aliens ((ju char jobu) (jvt char jobvt)
+	   (m integer dim0) (n integer dim1)
+	   (A array) (ld0 integer dim0) (S array) (u array)
+	   (ldu integer (dim1 u)) (vt array) (ldvt integer (dim1 vt)) (work array)
+	   (lwork integer (dim0 work)) (complex-only (rwork array)) (info integer 0)))
+
